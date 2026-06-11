9ca59012fcb074da2b53e2dd59f90e7535c0c71c
lgustavuh <gustavoreis5@msn.com>
2026-06-11 14:45:34 -0300

    update app.listen

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -88,7 +88,7 @@ async function bootstrap() {
   }
 
   const port = process.env.PORT || 4000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 Orkestria API running on port ${port}`);
 }
 
